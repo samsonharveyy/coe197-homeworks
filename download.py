037117c8a47b542ba246eedbd8dff473f911b478
samsonharvey <harvey.samson@eee.upd.edu.ph>
2022-05-02 20:21:40 +0800

    update trained weights

--- download.py
+++ download.py
@@ -9,7 +9,9 @@ def download_data():
 
 def download_trained():
     #single file, no extraction
-    #also in model_files folder in this repo (for 60 epochs only)
-    gdown.download("https://drive.google.com/uc?id=18q5n_IUcjeOrMf6q2LIX_MvD2ee6bsZm", "drinks-trained-weights.pth", quiet=False)
+    #(for 60 epochs only)
+    #gdown.download("https://drive.google.com/uc?id=18q5n_IUcjeOrMf6q2LIX_MvD2ee6bsZm", "drinks-trained-weights.pth", quiet=False)
 
+    #updated version, 80 epochs
+    gdown.download("https://drive.google.com/uc?id=1yF7MCj116-hc16xshxBjaJgU9zT_by8C", "drinks-trained-weights.pth", quiet=False)
 
